476b318bc0f02ae344d374b5561ac901cae9aa30
arszen123 <aliarszen@gmail.com>
2020-05-06 21:53:53 +0200

    Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appUrl: 'http://pp.devel/',
-  apiUrl: 'http://pp.devel:8080/',
+  appUrl: 'https://5eb2fb134e503489ea7d3e90--youthful-engelbart-7875d5.netlify.app/', // @TODO Change
+  apiUrl: 'https://pp-api123.herokuapp.com/',
   appBaseUrl: '/app',
 };
